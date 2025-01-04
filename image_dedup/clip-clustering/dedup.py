6fd730c34aa3347ed0b0545f267e8c37a75a2ef5
Carlos Catalan <ccatalan6@gatech.edu>
2025-01-04 21:34:30 +0800

    push dedup code

---
+++ image_dedup/clip-clustering/dedup.py
@@ -0,0 +1,65 @@
+from PIL import Image
+from matplotlib import pyplot as plt
+import requests
+
+from transformers import CLIPProcessor, CLIPModel
+
+model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
+processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
+
+import torch
+from pathlib import Path
+
+demo_directory = Path("./seacrowd-sample") #replace Path value to directory containing your images
+images_to_paths = {image_path.stem: image_path for image_path in demo_directory.iterdir()}
+
+images = [Image.open(path) for path in images_to_paths.values()]
+inputs = processor(images=images, return_tensors="pt", padding=True)
+
+with torch.no_grad():
+    outputs = model.get_image_features(**inputs)
+
+images_to_embeddings = {image_id: tensor_embedding.detach().numpy() for image_id, tensor_embedding in zip(images_to_paths.keys(), outputs)}
+
+import numpy as np
+from sklearn.cluster import DBSCAN
+from collections import defaultdict
+
+# tune eps to fit your needs
+clustering = DBSCAN(min_samples=2, eps=6).fit(np.stack(images_to_embeddings.values()))
+
+# postprocess cluster labels into groups of similar images
+image_id_communities = defaultdict(set)
+independent_image_ids = set()
+
+for image_id, cluster_idx in zip(images_to_paths.keys(), clustering.labels_):
+    cluster_idx = int(cluster_idx)
+    if cluster_idx == -1:
+        independent_image_ids.add(image_id)
+        continue
+
+    image_id_communities[cluster_idx].add(image_id)
+
+len(independent_image_ids) # = 10
+
+image_id_communities
+
+### Just shows the images considered as duplicates
+# for image_id_community in image_id_communities.values():
+#     for image_id in image_id_community:
+#         plt.figure()
+#         plt.imshow(Image.open(images_to_paths[image_id]))
+#         plt.show()
+
+### Just shows the images that aren't considered as duplicates
+# # images that have not got a cluster with similar images assigned to them
+# for image_id in independent_image_ids:
+#     plt.figure()
+#     plt.imshow(Image.open(images_to_paths[image_id]))
+#     plt.show()
+
+# print file names of images considered as duplicates
+print('image id communities', image_id_communities)
+
+# print file names of images not considered as duplicates
+# print('independent image ids', independent_image_ids)
